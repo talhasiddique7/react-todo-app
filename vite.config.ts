02e901e853ba9a73aa1fe59a6aed5277df28e2a0
talhasiddique7 <ts9740743@gmail.com>
2025-05-16 12:31:59 +0500

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-todo-app', // 🔁 Replace with your repo name
+  base: '/react-todo-app/', // 🔁 Replace with your repo name
   plugins: [react()],
   esbuild: {
     loader: "jsx"
